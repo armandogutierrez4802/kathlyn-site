be77f21eaf8b2b69ef12c02eb0463c20b98d8751
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2023-10-12 18:41:03 -0700

    Added header and footer with TailwindUI

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,113 @@
+// Font Awesome Imports
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
+//Tailwind UI Imports
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+
+// function classNames(...classes) {
+//     return classes.filter(Boolean).join(' ')
+// }
+
+
 export default function Navbar() {
     return (
-        <nav className='flex bg-gray-700'>
-            <h3>Kathlyn Gutierrez</h3>
-            <button>
-                <FontAwesomeIcon icon={faBars} className='text-3xl' />
-            </button>
-        </nav>
+        <Disclosure as="nav" className="bg-white shadow">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        <div className="flex h-16 justify-between">
+                            <div className="flex">
+                                <div className="flex flex-shrink-0 items-center">
+                                    {/* <img
+                                        className="h-8 w-auto"
+                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                        alt="Your Company"
+                                    /> */}
+                                    <div>Kathlyn Gutierrez</div>
+                                </div>
+                                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                                    {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                                    <a
+                                        href="#"
+                                        className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
+                                    >
+                                        Home
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                                    >
+                                        Order
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                                    >
+                                        About
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                                    >
+                                        FAQ
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="-mr-2 flex items-center sm:hidden">
+                                {/* Mobile menu button */}
+                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 pb-3 pt-2">
+                            {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+                            <Disclosure.Button
+                                as="a"
+                                href="#"
+                                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
+                            >
+                                Home
+                            </Disclosure.Button>
+                            <Disclosure.Button
+                                as="a"
+                                href="#"
+                                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                            >
+                                Order
+                            </Disclosure.Button>
+                            <Disclosure.Button
+                                as="a"
+                                href="#"
+                                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                            >
+                                About
+                            </Disclosure.Button>
+                            <Disclosure.Button
+                                as="a"
+                                href="#"
+                                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                            >
+                                FAQ
+                            </Disclosure.Button>
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )
+            }
+        </Disclosure >
     )
 }
